e3250d8e21d416eb36cbb46079a4dfd9052cd014
Frxzyl <105106411+Frxzyl@users.noreply.github.com>
2024-12-26 14:30:15 +0000

    Update Multiplication.cpp

--- Multiplication.cpp
+++ Multiplication.cpp
@@ -2,7 +2,8 @@
 #include <ctime>
 #include <random>
 #include <cstdlib>
-
+int totalquestions = 0;
+int correctanswers = 0;
 int main() {
 	while (true) {
 		srand(time(0));
@@ -11,12 +12,18 @@ int main() {
 		int guess = 0;
 		std::cout << number << " x " << number1 << " = ";
 		std::cin >> guess;
+		totalquestions++;
 		if (guess != number * number1) {
 			std::cout << "incorrect\n";
 		}
 		else {
 			std::cout << "Correct\n";
+			correctanswers++;
 		}
+
+		int percentage = (static_cast<double>(correctanswers) / totalquestions) * 100;
+		int numbers = (static_cast<int>(percentage));
+		std::cout << numbers << "%\n";
 
 	}
 }
